7912e8c8a93ec56b1686254f5178450b18da27cb
fredafei <964959033@qq.com>
2021-05-07 22:25:08 +0800

    更新router，删除无用代码

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,10 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
-import './index.css';
 import HelloWorld from './components/HelloWorld.vue';
 import Test from './components/Test.vue';
 import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
-const router = createRouter({
+export const router = createRouter({
   history,
   routes: [
     { path: '/', component: Test },
@@ -19,4 +18,3 @@ const router = createRouter({
     },
   ]
 });
-export default router;
