04b5d61e62c188a8a326ac8c1e2f69799e411e1c
fredafei <964959033@qq.com>
2021-05-07 22:24:46 +0800

    更新移动端适配

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,19 @@
 </template>
 
 <script language="ts">
+  import { ref, provide } from "vue";
+  import { router } from "./router";
   export default {
     name: 'App',
+    setup() {
+      const width = document.documentElement.clientWidth;
+      const visibleMenu = ref(width <= 500 ? false : true);
+      provide("visibleMenu", visibleMenu); // set
+      router.afterEach(() => {
+        if (width <= 500) {
+          visibleMenu.value = false;
+        }
+      });
+    },
   };
 </script>
