5fe034ab8644224f25f6c385e46a78153acee745
fredafei <964959033@qq.com>
2021-05-09 21:19:00 +0800

    更新visibleMenu

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,16 @@
   <router-view/>
 </template>
 
-<script language="ts">
-  import { ref, provide } from "vue";
-  import { router } from "./router";
+<script lang="ts">
+  import { ref, provide } from 'vue';
+  import { router } from './router';
+
   export default {
     name: 'App',
     setup() {
       const width = document.documentElement.clientWidth;
-      const visibleMenu = ref(width <= 500 ? false : true);
-      provide("visibleMenu", visibleMenu); // set
+      const visibleMenu = ref(false);
+      provide('visibleMenu', visibleMenu); // set
       router.afterEach(() => {
         if (width <= 500) {
           visibleMenu.value = false;
