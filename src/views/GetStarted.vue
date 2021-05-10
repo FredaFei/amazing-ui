e69670c16f1cfcc69c7450c663bf0051c7b53e1a
fredafei <964959033@qq.com>
2021-05-10 22:25:16 +0800

    更新开始、安装的文档

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,8 +1,11 @@
 <template>
-  <article>
-    <h1>get started</h1>
+  <article class="markdown-body">
+    <h1>快速开始</h1>
+    <p>一套轻量的web component，基于typeScript、Vue3.0 开发。</p>
+    <p>一套轻量的web component，基于typeScript、Vue3.0 开发。</p>
     <pre>
     <code>import {Button} from 'amazing-ui';</code>
   </pre>
+    <p>下一节: <a href="#/doc/install">安装</a></p>
   </article>
 </template>
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>install</h1>
+  <article>
+    <h1>安装</h1>
+    <pre><code>yarn add amazing-ui</code></pre>
+    <p>或</p>
+    <pre><code>npm install amazing-ui</code></pre>
+    <p>下一节: <a href="#/doc/button">Button</a></p>
+  </article>
 </template>
