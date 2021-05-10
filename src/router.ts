057e5d950112dded7aa6a2f36a6d1d7c4d4c5c42
fredafei <964959033@qq.com>
2021-05-10 22:11:55 +0800

    更新router

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,8 @@ import Button from './components/Button.vue';
 import Switch from './components/Switch.vue';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
+import GetStarted from './views/GetStarted.vue';
+import Install from './views/Install.vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -13,7 +15,9 @@ export const router = createRouter({
       path: '/doc',
       component: Doc,
       children: [
-        { path: '', component: Button },
+        { path: '', redirect: '/doc/get-started' },
+        { path: 'get-started', component: GetStarted },
+        { path: 'install', component: Install },
         { path: 'button', component: Button },
         { path: 'switch', component: Switch },
       ]
