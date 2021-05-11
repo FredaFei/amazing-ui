ed3fb75c3215212aeea824b320ee3081d3923182
fredafei <964959033@qq.com>
2021-05-11 22:05:21 +0800

    使用全局注册的Markdown组件

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,11 +1,3 @@
 <template>
   <Markdown path="../markdown/getStarted.md"/>
 </template>
-
-<script>
-  import Markdown from '../components/Markdown.vue';
-
-  export default {
-    components: { Markdown },
-  };
-</script>
