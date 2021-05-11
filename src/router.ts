438aec8e51e9ff3b26af042248311ae58a426426
fredafei <964959033@qq.com>
2021-05-11 22:13:46 +0800

    消除重复

--- src/router.ts
+++ src/router.ts
@@ -3,10 +3,12 @@ import Button from './components/Button.vue';
 import Switch from './components/Switch.vue';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue';
+import Markdown from './components/Markdown.vue';
+import { h } from 'vue';
 
 const history = createWebHashHistory();
+
+const md = (filename) => h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
 export const router = createRouter({
   history,
   routes: [
@@ -16,8 +18,8 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: '', redirect: '/doc/get-started' },
-        { path: 'get-started', component: GetStarted },
-        { path: 'install', component: Install },
+        { path: 'get-started', component: md('getStarted') },
+        { path: 'install', component: md('install') },
         { path: 'button', component: Button },
         { path: 'switch', component: Switch },
       ]
