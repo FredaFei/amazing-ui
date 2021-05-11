90f39adef41349e835b2e5a0146d39543bb026a0
fredafei <964959033@qq.com>
2021-05-11 22:05:06 +0800

    全局注册Markdown组件

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue';
 import { router } from './router';
 import './index.scss';
 import 'github-markdown-css';
+import Markdown from './components/Markdown.vue';
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+app.component('Markdown', Markdown);
