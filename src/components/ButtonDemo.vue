a557e8903347748a3f552884725535000e8e8b44
fredafei <964959033@qq.com>
2021-05-12 21:24:08 +0800

    删除无用代码

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="exp-box">
+  <div class="exp-content">
     <Demo :component="Button1Demo"/>
     <Demo :component="Button2Demo"/>
   </div>
@@ -16,49 +16,4 @@
   };
 </script>
 <style lang="scss" scoped>
-  $site-color: #314659;
-  $focus-color: #1890ff;
-  $site-border-color: #e8e8e8;
-  $light-color: #666;
-  .exp-box {
-    &.custom-icon {
-      .am-icon {font-size: 28px;}
-    }
-  }
-  .exp-section {
-    padding: 10px 20px 0;
-    border-radius: 4px;
-    background: white;
-    border: 1px solid #d6d6d6;
-    box-shadow: 1px 1px 0px #bababa;
-    overflow-x: auto;
-    p {padding-bottom: 10px;}
-    & + .exp-section {
-      margin-top: 20px;
-      font-size: 15px;
-    }
-    &-btn {
-      background: #66e2d5;
-      border-color: #000;
-      color: #000;
-      &:hover {
-        border-color: #000;
-        background: rgba(#66e2d5, 0.9);
-      }
-    }
-  }
-  .code-box {
-    color: $light-color;
-    margin-top: 20px;
-    padding: 10px 0;
-    border-top: 1px solid $site-border-color;
-    .code-title {
-      display: flex;
-      align-items: center;
-      color: $focus-color;
-      font-size: 16px;
-      cursor: pointer;
-    }
-    .am-icon {font-size: 30px;margin-right: 6px;}
-  }
 </style>
