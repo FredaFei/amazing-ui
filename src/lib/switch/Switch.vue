fcb5dc07f4a72f4b5cc3a5c19d657e9090c15a3e
fredafei <964959033@qq.com>
2021-06-11 23:50:25 +0800

    更新switch组件样式

--- src/lib/switch/Switch.vue
+++ src/lib/switch/Switch.vue
@@ -1,7 +1,77 @@
 <template>
-  <h1>switch</h1>
+  <label class="switch-wrap active" @click={onClick}>
+    <span class="core active">
+      <span class="ripple active"></span>
+    </span>
+  </label>
 </template>
 <script lang="ts">
   export default {
+    props: {},
+    setup() {
+      const onClick = () => {
+      };
+      return { onClick };
+    },
   };
 </script>
+<style lang="scss" scoped>
+  @keyframes ripple {
+    to {
+      transform: scale(1);
+      opacity: 0;
+    }
+  }
+  @mixin ripple {
+    animation: ripple 0.65s ease-out;
+  }
+  $blue: #40a9ff;
+  $color: #333;
+  .switch-wrap {
+    display: inline-flex;
+    vertical-align: middle;
+    width: 40px;
+    height: 14px;
+    border-radius: 8px;
+    background: rgba($color, 0.14);
+    cursor: pointer;
+    position: relative;
+    transition: background 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+    &.active {
+      background: rgba(#2196f3, 0.6);
+    }
+    &.disabled {
+      opacity: 0.45;
+    }
+    .core {
+      position: absolute;
+      top: 50%;
+      margin-top: -10px;
+      width: 20px;
+      height: 20px;
+      box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+      border-radius: 50%;
+      background: #fff;
+      transform: translateX(0%);
+      transition: all 0.2s linear;
+      &.active {
+        background: $blue;
+        transform: translateX(100%);
+      }
+      .ripple {
+        position: absolute;
+        border-radius: 50%;
+        left: 50%;
+        top: 50%;
+        transform: scale(0);
+        pointer-events: none;
+        opacity: 1;
+        background: inherit;
+        &.active {
+          @include ripple;
+        }
+      }
+    }
+  }
+</style>
