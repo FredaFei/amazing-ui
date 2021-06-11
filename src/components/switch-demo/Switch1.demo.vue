447efce69ae0c9f4aad80b22b2fef09ffd12b4b4
fredafei <964959033@qq.com>
2021-06-11 23:22:28 +0800

    更新switch组件文档demo

---
+++ src/components/switch-demo/Switch1.demo.vue
@@ -0,0 +1,18 @@
+<demo>
+  常规使用
+</demo>
+<template>
+  <div>
+    <Switch/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Switch from '../../lib/switch/Switch.vue';
+
+  export default {
+    components: {
+      Switch
+    }
+  };
+</script>
---
+++ src/components/switch-demo/SwitchDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exp-content">
+    <Demo :component="Switch1Demo"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Switch1Demo from './Switch1.demo.vue';
+  import Demo from '../Demo.vue';
+
+  export default {
+    components: { Demo },
+    setup() {return { Switch1Demo, };}
+  };
+</script>
+<style lang="scss" scoped>
+</style>
