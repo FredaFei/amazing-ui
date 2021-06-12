be93d0deaae35ef0eef568ec18b08a3c19e9573f
fredafei <964959033@qq.com>
2021-06-12 22:01:05 +0800

    更新组件库中的文档导航栏名称

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -17,19 +17,19 @@
           <h2>组件</h2>
           <ol>
             <li>
-              <router-link to="/doc/icon">Icon 组件</router-link>
+              <router-link to="/doc/icon">Icon 图标</router-link>
             </li>
             <li>
-              <router-link to="/doc/button">Button 组件</router-link>
+              <router-link to="/doc/button">Button 按钮</router-link>
             </li>
             <li>
-              <router-link to="/doc/switch">Switch 组件</router-link>
+              <router-link to="/doc/switch">Switch 开关</router-link>
             </li>
             <li>
-              <router-link to="/doc/tabs">Tabs 组件</router-link>
+              <router-link to="/doc/tabs">Tabs 标签页</router-link>
             </li>
             <li>
-              <router-link to="/doc/dialog">Dialog 组件</router-link>
+              <router-link to="/doc/dialog">Dialog 对话框</router-link>
             </li>
           </ol>
         </aside>
