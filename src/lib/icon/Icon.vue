359574cc5b2c7d045e91254e2ce9f62d01d571d5
fredafei <964959033@qq.com>
2021-06-12 18:02:17 +0800

    添加Icon组件

---
+++ src/lib/icon/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg class="am-icon" :class="{'am-icon-spin': spin||(name === 'loading')}" :style="rotateStyle" v-bind="$attrs">
+    <use :xlink:href="`#i-${name}`"/>
+  </svg>
+</template>
+<script lang="ts">
+  import './svg.js';
+  import { computed } from 'vue';
+
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      spin: Boolean,
+      rotate: Number
+    },
+    setup(props, context) {
+      const rotateStyle = computed(() => {
+        return props.rotate && {
+          transform: `rotate(${props.rotate}deg)`,
+          transition: `transform .25s ease`
+        };
+      });
+      return { rotateStyle };
+    }
+  };
+</script>
+<style lang="scss">
+  @import '../../style/_var.scss';
+  #{component(icon)} {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.1em;
+    fill: currentColor;
+    overflow: hidden;
+    &-spin {
+      @include spin;
+    }
+  }
+</style>
