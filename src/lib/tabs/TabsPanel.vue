2ac5ad1cd0e53da2b52f12dfbf04635c3ba0ef50
fredafei <964959033@qq.com>
2021-06-12 14:35:51 +0800

    更新tabsPanel组件,添加props name定义

--- src/lib/tabs/TabsPanel.vue
+++ src/lib/tabs/TabsPanel.vue
@@ -2,6 +2,13 @@
   <div><slot/></div>
 </template>
 <script lang="ts">
-  export default {}
+  export default {
+    props:{
+      name: {
+        type: String,
+        required: true
+      },
+    }
+  }
 </script>
 <style lang="scss" scoped></style>
