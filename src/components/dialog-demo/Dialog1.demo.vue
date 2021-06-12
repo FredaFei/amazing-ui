6de7ab3d4b33e8057e8b04e8e4005804b6e4b894
fredafei <964959033@qq.com>
2021-06-12 21:59:46 +0800

    添加dialog组件文档demo

---
+++ src/components/dialog-demo/Dialog1.demo.vue
@@ -0,0 +1,26 @@
+<demo>
+  常规使用
+</demo>
+<template>
+  <div>
+    <Button @click="onClick">点我</Button>
+    <Dialog></Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+  import Button from '../../lib/button/Button.vue';
+  import Dialog from '../../lib/dialog/Dialog.vue';
+
+  export default {
+    components: {
+      Button,Dialog
+    },
+    setup() {
+      const onClick = () => {
+        console.log('click');
+      };
+      return { onClick };
+    }
+  };
+</script>
---
+++ src/components/dialog-demo/DialogDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exp-content">
+    <Demo :component="Dialog1Demo"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Dialog1Demo from './Dialog1.demo.vue';
+  import Demo from '../Demo.vue';
+
+  export default {
+    components: { Demo },
+    setup() {return { Dialog1Demo };}
+  };
+</script>
+<style lang="scss" scoped>
+</style>
