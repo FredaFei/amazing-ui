3173cc0c2d8647910094777191262c29035f7872
fredafei <964959033@qq.com>
2021-06-12 21:58:42 +0800

    添加dialog组件文档路由

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import IconDemo from './components/icon-demo/IconDemo.vue';
 import ButtonDemo from './components/button-demo/ButtonDemo.vue';
 import SwitchDemo from './components/switch-demo/SwitchDemo.vue';
 import TabsDemo from './components/tabs-demo/TabsDemo.vue';
+import DialogDemo from './components/dialog-demo/DialogDemo.vue';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import Markdown from './components/Markdown.vue';
@@ -26,6 +27,7 @@ export const router = createRouter({
         { path: 'button', component: ButtonDemo },
         { path: 'switch', component: SwitchDemo },
         { path: 'tabs', component: TabsDemo },
+        { path: 'dialog', component: DialogDemo },
       ]
     },
   ]
