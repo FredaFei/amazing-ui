88461a707d93c0f8e819fa2bf8f8190537575586
fredafei <964959033@qq.com>
2021-06-12 09:42:27 +0800

    更新switch组件文档

--- src/components/switch-demo/SwitchDemo.vue
+++ src/components/switch-demo/SwitchDemo.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="exp-content">
     <Demo :component="Switch1Demo"/>
+    <Demo :component="Switch2Demo"/>
   </div>
 </template>
 
 <script lang="ts">
   import Switch1Demo from './Switch1.demo.vue';
+  import Switch2Demo from './Switch2.demo.vue';
   import Demo from '../Demo.vue';
 
   export default {
     components: { Demo },
-    setup() {return { Switch1Demo, };}
+    setup() {return { Switch1Demo, Switch2Demo };}
   };
 </script>
 <style lang="scss" scoped>
