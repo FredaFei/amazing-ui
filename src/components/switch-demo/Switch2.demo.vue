dc1007898c0af5f6fef0da0a3f6436774a32a2cb
fredafei <964959033@qq.com>
2021-06-12 09:42:09 +0800

    更新switch组件disabled状态下的文档demo

---
+++ src/components/switch-demo/Switch2.demo.vue
@@ -0,0 +1,25 @@
+<demo>
+  disabled 状态
+</demo>
+<template>
+  <div>
+    <Switch :value="false" disabled="true"/>
+    <Switch :value="true" disabled="true" style="margin-left: 20px"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Switch from '../../lib/switch/Switch.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      Switch
+    },
+    setup() {
+      const checked = ref(false);
+      const disabled = ref(true);
+      return { checked, disabled };
+    },
+  };
+</script>
