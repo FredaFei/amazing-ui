b284734163013ebefebb8098d5091538df08fcb9
fredafei <964959033@qq.com>
2021-06-12 10:35:45 +0800

    更新tabs组件文档demo

---
+++ src/components/tabs-demo/Tabs1.demo.vue
@@ -0,0 +1,25 @@
+<demo>
+  基础应用
+</demo>
+<template>
+  <div>
+    <Tabs>
+      <TabsPanel title="tabs 1">panel 1</TabsPanel>
+      <TabsPanel title="tabs 2">panel 2</TabsPanel>
+    </Tabs>
+  </div>
+</template>
+
+<script lang="ts">
+  import Tabs from '../../lib/tabs/Tabs.vue';
+  import TabsPanel from '../../lib/tabs/TabsPanel.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      Tabs, TabsPanel
+    },
+    setup() {
+    },
+  };
+</script>
---
+++ src/components/tabs-demo/TabsDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exp-content">
+    <Demo :component="Tabs1Demo"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Tabs1Demo from './Tabs1.demo.vue';
+  import Demo from '../Demo.vue';
+
+  export default {
+    components: { Demo },
+    setup() {return { Tabs1Demo };}
+  };
+</script>
+<style lang="scss" scoped>
+</style>
