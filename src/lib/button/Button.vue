593fb036de84afde1a5303db7659f9fc2cbebedb
fredafei <964959033@qq.com>
2021-06-12 23:50:02 +0800

    完善button组件,添加主题接口

--- src/lib/button/Button.vue
+++ src/lib/button/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="am-button-wrapper">
+  <button class="am-button-wrapper" :class="[`am-button-${theme}`]">
     <div v-if="icon" class="am-button-body" :class="{[`icon-${iconPosition}`]: true}">
       <Icon :name="icon" class="icon-name"/>
       <span class="am-button-content"><slot/></span>
@@ -21,6 +21,13 @@
         validator(value) {
           return value === 'left' || value === 'right';
         }
+      },
+      theme: {
+        type: String,
+        default: 'default',
+        validator(value) {
+          return ['default', 'primary', 'danger'].includes(value);
+        }
       }
     }
   };
@@ -28,30 +35,52 @@
 <style lang="scss">
   @import '../../style/_var.scss';
   .am-button {
-    &-wrapper {
-      vertical-align: middle;
-      font-size: $button-fontSize;
-      line-height: $button-height;
-      padding: 0 1em;
-      border-radius: $border-radius;
+    &-default {
       border: 1px solid $border-color;
       background: $button-bg;
-      outline: none;
-      overflow: hidden;
-      cursor: pointer;
-      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-      position: relative;
+      &:hover, &:focus {
+        border-color: $border-color-hover;
+      }
       &:disabled {
-        cursor: not-allowed;
+        color: $light-color;
         background-color: #f5f5f5;
         border-color: #d9d9d9;
         &:hover {
           border-color: #d9d9d9;
         }
       }
-      &:hover, &:focus {
-        color: $blue;
-        border-color: $border-color-hover;
+    }
+    &-primary {
+      color: $white;
+      background: $darkblue;
+      border: 1px solid $darkblue;
+      &:hover, &:focus, &:disabled {
+        background: $blue;
+        border-color: $blue;
+      }
+    }
+    &-danger {
+      color: $white;
+      background: $error-color;
+      border: 1px solid $error-color;
+      &:hover, &:focus, &:disabled {
+        background: $error-color-hover;
+        border-color: $error-color-hover;
+      }
+    }
+    &-wrapper {
+      vertical-align: middle;
+      font-size: $button-fontSize;
+      line-height: $button-height;
+      padding: 0 1em;
+      outline: none;
+      overflow: hidden;
+      cursor: pointer;
+      border-radius: $border-radius;
+      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+      position: relative;
+      &:disabled {
+        cursor: not-allowed;
       }
       &:active {
         background-color: $border-active-bg;
