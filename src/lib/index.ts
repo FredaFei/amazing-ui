91ecd4cc3e1646e1756c5f4edc8d0bd29afde684
fredafei <964959033@qq.com>
2021-06-13 23:31:12 +0800

    更新组件库导出

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+import '../style/reset.scss';
+
+export { default as Icon } from './icon/Icon.vue';
+export { default as Switch } from './switch/Switch.vue';
+export { default as Button } from './button/Button.vue';
+export { default as ButtonGroup } from './button/ButtonGroup.vue';
+export { default as Dialog } from './dialog/Dialog.vue';
+export { default as Tabs } from './tabs/Tabs.vue';
+export { default as TabsPanel } from './tabs/TabsPanel.vue';
+export { openDialog, alert, confirm, info } from './dialog/openDialog';
