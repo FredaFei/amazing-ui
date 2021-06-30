66c8846e7e4a2ccd96783bb009da53b810f4cdba
fredafei <964959033@qq.com>
2021-06-30 23:23:09 +0800

    全局注册文档样式组件

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import './index.scss';
 import 'amazing-ui-vue3/dist/lib/index.css';
 import 'github-markdown-css';
 import Markdown from './components/Markdown.vue';
+import Space from './components/Space.vue';
+
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
 app.component('Markdown', Markdown);
+app.component('Space', Space);
