b8ac9c23f39468b02127984ed0e7ae2818d8876b
fredafei <964959033@qq.com>
2021-06-30 23:23:59 +0800

    更新Tabs组件下标线尺寸文档示例

---
+++ src/components/tabs-demo/Tabs3.demo.vue
@@ -0,0 +1,28 @@
+<demo>
+  指定下标线尺寸
+</demo>
+<template>
+  <div>
+    <Space/>
+    <Tabs v-model:selected="current" :lineWidthOrHeight="lineWidthOrHeight">
+      <TabsPanel title="tabs 1" name="1">panel 1</TabsPanel>
+      <TabsPanel title="tabs 1000" name="2">panel 2</TabsPanel>
+    </Tabs>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Tabs, TabsPanel, Button, ButtonGroup } from 'amazing-ui-vue3';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      Tabs, TabsPanel, Button, ButtonGroup
+    },
+    setup() {
+      const lineWidthOrHeight = ref(20);
+      const current = ref('1');
+      return { current, lineWidthOrHeight };
+    },
+  };
+</script>
--- src/components/tabs-demo/TabsDemo.vue
+++ src/components/tabs-demo/TabsDemo.vue
@@ -2,17 +2,19 @@
   <div class="exp-content">
     <Demo :component="Tabs1Demo"/>
     <Demo :component="Tabs2Demo"/>
+    <Demo :component="Tabs3Demo"/>
   </div>
 </template>
 
 <script lang="ts">
   import Tabs1Demo from './Tabs1.demo.vue';
   import Tabs2Demo from './Tabs2.demo.vue';
+  import Tabs3Demo from './Tabs3.demo.vue';
   import Demo from '../Demo.vue';
 
   export default {
     components: { Demo },
-    setup() {return { Tabs1Demo, Tabs2Demo, };}
+    setup() {return { Tabs1Demo, Tabs2Demo, Tabs3Demo };}
   };
 </script>
 <style lang="scss" scoped>
