39c4717d54072acc63fc496d156e06ec23054274
fredafei <964959033@qq.com>
2021-07-01 23:52:19 +0800

    添加radio 组件文档示例

---
+++ src/components/radio-demo/Radio2.demo.vue
@@ -0,0 +1,29 @@
+<demo>
+  disabled 状态
+</demo>
+<template>
+  <div>
+    <Radio v-model:checked="checkedValue" name="fruit" value="apple">apple</Radio>
+    <Radio v-model:checked="checkedValue" name="fruit" value="orange">orange</Radio>
+    <Radio v-model:checked="checkedValue" name="fruit" value="banana" disabled>banana</Radio>
+  </div>
+</template>
+
+<script lang="ts">
+  import { ref } from 'vue';
+  import Radio from '../../lib/radio/Radio.vue';
+
+  export default {
+    components: {
+      Radio
+    },
+    setup() {
+      const checkedValue = ref('apple');
+      const onChange = (value) => {
+        // console.log('122');
+        // checkedValue.value = value;
+      };
+      return { onChange, checkedValue };
+    }
+  };
+</script>
