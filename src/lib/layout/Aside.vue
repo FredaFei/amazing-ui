1ac6e8c32414cea16d1d0e73658c9cf998601b1f
fredafei <964959033@qq.com>
2021-07-04 20:45:17 +0800

    更新aside组件，删除无用代码

--- src/lib/layout/Aside.vue
+++ src/lib/layout/Aside.vue
@@ -12,29 +12,4 @@
   };
 </script>
 <style lang="scss">
-  @import "../../style/_var.scss";
-  #{component(layout)} {
-    display: flex;
-    flex-direction: column;
-    & > & {
-      flex-grow: 1;
-    }
-    &-hasAside {
-      flex-direction: row;
-      #{component(layout)} {
-        flex-direction: column;
-      }
-    }
-  }
-  #{component(header)} {
-    flex-grow: 0;
-    flex-shrink: 1;
-  }
-  #{component(footer)} {
-    flex-grow: 0;
-    flex-shrink: 1;
-  }
-  #{component(main)} {
-    flex: 1;
-  }
 </style>
