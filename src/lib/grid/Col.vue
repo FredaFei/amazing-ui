95cae2ef6d932273e2b35e8ef94085f6d504103c
fredafei <964959033@qq.com>
2021-07-04 20:47:46 +0800

    添加grid组件

---
+++ src/lib/grid/Col.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="am-col">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  defineComponent;
+  export default {
+    name: 'AmCol'
+  };
+</script>
+<style lang="scss">
+</style>
---
+++ src/lib/grid/Row.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="am-row">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  defineComponent;
+  export default {
+    name: 'AmRow'
+  };
+</script>
+<style lang="scss">
+</style>
