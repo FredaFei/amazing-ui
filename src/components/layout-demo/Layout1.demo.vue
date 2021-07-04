b552f752aef061d087233acca71192589614d476
fredafei <964959033@qq.com>
2021-07-04 20:22:10 +0800

    添加layout 组件基础示例

---
+++ src/components/layout-demo/Layout1.demo.vue
@@ -0,0 +1,30 @@
+<demo>
+  常规使用
+</demo>
+<template>
+  <p>上中下布局</p>
+  <Layout class="custom-layout">
+    <Header>Header</Header>
+    <Main>Content</Main>
+    <Footer>Footer</Footer>
+  </Layout>
+</template>
+
+<script lang="ts">
+  import { Layout, Header, Main, Footer } from '../../lib/layout/index';
+
+  export default {
+    components: {
+      Layout, Header, Main, Footer
+    },
+    setup() {
+      const onClick = () => {
+        console.log('click');
+      };
+      return { onClick };
+    }
+  };
+</script>
+<style lang="scss">
+  @import "./index.scss";
+</style>
---
+++ src/components/layout-demo/LayoutDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exp-content">
+    <Demo :component="Layout1Demo"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Layout1Demo from './Layout1.demo.vue';
+  import Demo from '../Demo.vue';
+
+  export default {
+    components: { Demo },
+    setup() {return { Layout1Demo };}
+  };
+</script>
+<style lang="scss" scoped>
+</style>
