6a63954e5b68eaa1e7a5aaf02c20188d6cebe519
fredafei <964959033@qq.com>
2021-07-04 20:51:32 +0800

    添加grid 组件示例

---
+++ src/components/grid-demo/Grid1.demo.vue
@@ -0,0 +1,18 @@
+<demo>
+</demo>
+<template>
+</template>
+
+<script lang="ts">
+  import Row from '../../lib/grid/Row.vue';
+  import Col from '../../lib/grid/Col.vue';
+
+  export default {
+    components: {
+      Row, Col
+    },
+  };
+</script>
+<style lang="scss">
+  @import "./index.scss";
+</style>
---
+++ src/components/grid-demo/GridDemo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="exp-content">
+    <Demo :component="Layout1Demo"/>
+    <Demo :component="Layout2Demo"/>
+    <Demo :component="Layout3Demo"/>
+    <Demo :component="Layout4Demo"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Layout1Demo from './Grid1.demo.vue';
+  import Layout2Demo from './Layout2.demo.vue';
+  import Layout3Demo from './Layout3.demo.vue';
+  import Layout4Demo from './Layout4.demo.vue';
+  import Demo from '../Demo.vue';
+
+  export default {
+    components: { Demo },
+    setup() {return { Layout1Demo, Layout2Demo, Layout3Demo, Layout4Demo, };}
+  };
+</script>
+<style lang="scss" scoped>
+</style>
