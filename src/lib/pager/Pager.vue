f8662b8474f7a09b12efad053235ab5653f78bc4
fredafei <964959033@qq.com>
2021-07-10 12:44:16 +0800

    初始化Pager 组件

---
+++ src/lib/pager/Pager.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="am-pager"></div>
+</template>
+<script lang="ts">
+  export default {
+    name: 'AmPager',
+  };
+</script>
+
+<style lang="scss">
+  @import '../../style/_var.scss';
+</style>
