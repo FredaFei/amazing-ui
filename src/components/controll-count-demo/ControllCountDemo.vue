49122a1570f5e3e7619aac508f8d4da9a9afeef7
fredafei <964959033@qq.com>
2021-07-10 12:35:28 +0800

    完善controll-count 组件文档

--- src/components/controll-count-demo/ControllCountDemo.vue
+++ src/components/controll-count-demo/ControllCountDemo.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="exp-content">
-    <Demo :component="Checkbox1Demo"/>
+    <Demo :component="ControllCount1Demo"/>
+    <Demo :component="ControllCount2Demo"/>
+    <Demo :component="ControllCount3Demo"/>
   </div>
 </template>
 
 <script lang="ts">
-  import ControllCount1Demo from './ControllCount1.demo';
+  import ControllCount1Demo from './ControllCount1.demo.vue';
+  import ControllCount2Demo from './ControllCount2.demo.vue';
+  import ControllCount3Demo from './ControllCount3.demo.vue';
   import Demo from '../Demo.vue';
 
   export default {
     components: { Demo },
-    setup() {return { ControllCount1Demo, };}
+    setup() {return { ControllCount1Demo, ControllCount2Demo, ControllCount3Demo };}
   };
 </script>
 <style lang="scss" scoped>
