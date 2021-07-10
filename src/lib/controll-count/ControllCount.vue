8f899a762e1a4865a87b5859eb0780123bcc494f
fredafei <964959033@qq.com>
2021-07-10 09:22:36 +0800

    添加controll-count 组件

---
+++ src/lib/controll-count/ControllCount.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>controll count</div>
+</template>
+<script lang="ts">
+  export default {
+    name: 'AmControllCount',
+  };
+</script>
+<style lang="scss">
+  @import '../../style/_var.scss';
+
+</style>
