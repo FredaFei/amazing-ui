d6c9dbd52d41e28db14596ca722b90a711c3957c
fredafei <964959033@qq.com>
2021-07-10 09:16:34 +0800

    更新checkbox 组件文档全选例子

--- src/components/checkbox-demo/Checkbox2.demo.vue
+++ src/components/checkbox-demo/Checkbox2.demo.vue
@@ -1,25 +1,48 @@
 <demo>
-  disabled 状态
+  全选
 </demo>
 <template>
   <div>
-    <Checkbox :value="false" disabled="true"/>
-    <Checkbox :value="true" disabled="true" style="margin-left: 20px"/>
+    <div>
+      <Checkbox v-model:checked="checkedAll" @change="onChange" :indeterminate="indeterminate">全选水果</Checkbox>
+    </div>
+    <Checkbox v-model:checked="checkedNames" @change="onChange" :name="i.name" :value="i.value" v-for="i in options">{{i.value}}</Checkbox>
   </div>
 </template>
 
 <script lang="ts">
   import Checkbox from '../../lib/checkbox/Checkbox.vue';
-  import { ref } from 'vue';
+  import { ref, computed, } from 'vue';
 
   export default {
     components: {
       Checkbox
     },
     setup() {
-      const checked = ref(false);
-      const disabled = ref(true);
-      return { checked, disabled };
-    },
+      const options = [
+        { name: 'fruit', value: 'apple' },
+        { name: 'fruit', value: 'peach' },
+        { name: 'fruit', value: 'orange' },
+        { name: 'fruit', value: 'pear' },
+        { name: 'fruit', value: 'banana' }
+      ];
+      const checkedNames = ref([]);
+      const checkedAll = computed({
+        get() {
+          return checkedNames.value.length === options.length;
+        },
+        set(value) {
+          checkedNames.value = value ? options.map(it => it.value) : [];
+        }
+      });
+      const indeterminate = computed(() => {
+        const doneLength = checkedNames.value.length;
+        return doneLength > 0 && (options.length > doneLength);
+      });
+      const onChange = (value) => {
+        console.log('onchange ', value);
+      };
+      return { checkedNames, options, checkedAll, indeterminate, onChange };
+    }
   };
 </script>
