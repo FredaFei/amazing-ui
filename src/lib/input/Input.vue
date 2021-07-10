e8fd797340e6f48f808f001ee616afbc84338c62
fredafei <964959033@qq.com>
2021-07-10 13:33:50 +0800

    初始化Input 组件

---
+++ src/lib/input/Input.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="am-input"></div>
+</template>
+<script lang="ts">
+  export default {
+    name: 'AmInput',
+  };
+</script>
+<style lang="scss">
+  @import '../../style/_var.scss';
+
+</style>
