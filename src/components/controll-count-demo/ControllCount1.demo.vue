c3b26298f979d895627998417c74721aa82917ae
fredafei <964959033@qq.com>
2021-07-10 09:28:15 +0800

    初始化controll-count 组件文档

---
+++ src/components/controll-count-demo/ControllCount1.demo.vue
@@ -0,0 +1,23 @@
+<demo>
+  基础应用
+</demo>
+<template>
+  <div>
+    <ControllCount></ControllCount>
+  </div>
+</template>
+
+<script lang="ts">
+  import ControllCount from '../../lib/controll-count/ControllCount.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      ControllCount
+    },
+    setup() {
+      const checked = ref(false);
+      return { checked };
+    },
+  };
+</script>
---
+++ src/components/controll-count-demo/ControllCountDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exp-content">
+    <Demo :component="Checkbox1Demo"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import ControllCount1Demo from './ControllCount1.demo';
+  import Demo from '../Demo.vue';
+
+  export default {
+    components: { Demo },
+    setup() {return { ControllCount1Demo, };}
+  };
+</script>
+<style lang="scss" scoped>
+</style>
