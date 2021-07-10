faad64b744cba5ae1e2d3e564d651cf1998122b6
fredafei <964959033@qq.com>
2021-07-10 09:17:04 +0800

    更新checkbox 组件文档手动更新例子

---
+++ src/components/checkbox-demo/Checkbox4.demo.vue
@@ -0,0 +1,26 @@
+<demo>
+  @update:checked事件，手动更新
+</demo>
+<template>
+  <div>
+    <Checkbox :checked="checked" @update:checked="onChange" name="fruit" value="orange">orange</Checkbox>
+  </div>
+</template>
+
+<script lang="ts">
+  import Checkbox from '../../lib/checkbox/Checkbox.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      Checkbox
+    },
+    setup() {
+      const checked = ref(false);
+      const onChange = (value) => {
+        checked.value = value;
+      };
+      return { checked, onChange };
+    },
+  };
+</script>
