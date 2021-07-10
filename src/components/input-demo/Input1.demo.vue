75cee393cd5805c8eba9c39ad6005a15d8c13927
fredafei <964959033@qq.com>
2021-07-10 13:37:55 +0800

    初始化添加input 组件文档例子

---
+++ src/components/input-demo/Input1.demo.vue
@@ -0,0 +1,23 @@
+<demo>
+  基础应用
+</demo>
+<template>
+  <div>
+    <Input/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Input from '../../lib/input/Input.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      Input
+    },
+    setup() {
+      const count = ref(1);
+      return { count };
+    },
+  };
+</script>
