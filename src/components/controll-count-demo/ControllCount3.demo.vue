eb4f3a07f81bbc6f82ab4aaf77ba4a7c1428bad2
fredafei <964959033@qq.com>
2021-07-10 12:34:55 +0800

    初始化controll-count 组件文档，disabled

---
+++ src/components/controll-count-demo/ControllCount3.demo.vue
@@ -0,0 +1,23 @@
+<demo>
+  disable 状态
+</demo>
+<template>
+  <div style="width: 140px">
+    <ControllCount v-model:count="count" :disabled="true"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import ControllCount from '../../lib/controll-count/ControllCount.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      ControllCount
+    },
+    setup() {
+      const count = ref(2);
+      return { count };
+    },
+  };
+</script>
