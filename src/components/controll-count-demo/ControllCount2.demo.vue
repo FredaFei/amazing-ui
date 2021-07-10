222f56d48e87061f189c2fcbfd568f151db4caee
fredafei <964959033@qq.com>
2021-07-10 12:29:06 +0800

    初始化controll-count 组件文档，input不可输入

---
+++ src/components/controll-count-demo/ControllCount2.demo.vue
@@ -0,0 +1,23 @@
+<demo>
+  input 不可输入
+</demo>
+<template>
+  <div style="width: 140px">
+    <ControllCount v-model:count="count" read-only/>
+  </div>
+</template>
+
+<script lang="ts">
+  import ControllCount from '../../lib/controll-count/ControllCount.vue';
+  import { ref } from 'vue';
+
+  export default {
+    components: {
+      ControllCount
+    },
+    setup() {
+      const count = ref(2);
+      return { count };
+    },
+  };
+</script>
