38bcab7eb90aff0caa5dea2712621026da155f8b
fredafei <964959033@qq.com>
2021-07-13 22:38:50 +0800

    更新Button组件文档，引入markdown

--- src/components/button-demo/ButtonDemo.vue
+++ src/components/button-demo/ButtonDemo.vue
@@ -3,6 +3,7 @@
     <Demo :component="Button1Demo"/>
     <Demo :component="Button2Demo"/>
     <Demo :component="Button3Demo"/>
+    <Markdown v-bind:content="Doc"/>
   </div>
 </template>
 
@@ -11,10 +12,11 @@
   import Button2Demo from './Button2.demo.vue';
   import Button3Demo from './Button3.demo.vue';
   import Demo from '../Demo.vue';
+  import Doc from './Button.doc.md';
 
   export default {
     components: { Demo },
-    setup() {return { Button1Demo, Button2Demo,Button3Demo };}
+    setup() {return { Button1Demo, Button2Demo, Button3Demo, Doc };}
   };
 </script>
 <style lang="scss" scoped>
