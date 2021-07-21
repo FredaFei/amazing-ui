07b8f0aba442b3d0a063b00eeafd9575b39972b6
fredafei <964959033@qq.com>
2021-07-21 22:28:39 +0800

    更新Grid组件文档，引入markdown

---
+++ src/components/grid-demo/Grid.doc.md
@@ -0,0 +1,16 @@
+### API
+### Row
+
+| 参数         |说明        |类型 |默认值 |可选值 |
+| ------------ | :----------------|:------- | :----- | :----- |
+| gutter         |设置栅格间隔 |Number |0 |—— |
+| align |flex 布局下的垂直对齐方式 |String |left |`right/center/spaceBetween/spaceAround` |
+| verticalAlign      |设置按钮失效状态  |String |top |`center/bottom` |
+
+### Col
+
+| 参数         |说明        |类型 |默认值 |可选值 |
+| ------------ | :----------------|:------- | :----- | :----- |
+| span |栅格占位格数，设置为 0 时相当于设置`display: none` |Number |0 |—— |
+| gutter      |设置栅格间隔  |Number |0 |—— |
+| offset      |栅格向右偏移的格数  |Number |0 |—— |
--- src/components/grid-demo/GridDemo.vue
+++ src/components/grid-demo/GridDemo.vue
@@ -4,6 +4,7 @@
     <Demo :component="Grid2Demo"/>
     <Demo :component="Grid3Demo"/>
     <Demo :component="Grid4Demo"/>
+    <Markdown v-bind:content="Doc"/>
   </div>
 </template>
 
@@ -13,10 +14,11 @@
   import Grid3Demo from './Grid3.demo.vue';
   import Grid4Demo from './Grid4.demo.vue';
   import Demo from '../Demo.vue';
+  import Doc from './Grid.doc.md';
 
   export default {
     components: { Demo },
-    setup() {return { Grid1Demo, Grid2Demo, Grid3Demo, Grid4Demo };}
+    setup() {return { Grid1Demo, Grid2Demo, Grid3Demo, Grid4Demo, Doc };}
   };
 </script>
 <style lang="scss" scoped>
