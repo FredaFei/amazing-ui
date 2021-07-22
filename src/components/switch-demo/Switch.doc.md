67e8dcc7a73d007a0a6e25fc7ac888d58197a4c2
fredafei <964959033@qq.com>
2021-07-22 23:09:53 +0800

    更新Switch组件文档，引入markdown

---
+++ src/components/switch-demo/Switch.doc.md
@@ -0,0 +1,12 @@
+### API
+
+| 参数         |说明        |类型 |默认值 |可选值 |
+| ------------ | :----------------|:------- | :----- | :----- |
+| value |受控模式下的值 | Boolean |false |true |
+| disabled      |设置失效状态  |Boolean |false |true |
+
+### Events
+
+| 事件名称 |      说明      | 类型 |
+| -------- | :------------: | -------: |
+| on-update:value   | 当 value 改变时触发的回调函数 | (value: Boolean) => void |
--- src/components/switch-demo/SwitchDemo.vue
+++ src/components/switch-demo/SwitchDemo.vue
@@ -2,6 +2,7 @@
   <div class="exp-content">
     <Demo :component="Switch1Demo"/>
     <Demo :component="Switch2Demo"/>
+    <Markdown v-bind:content="Doc"/>
   </div>
 </template>
 
@@ -9,10 +10,11 @@
   import Switch1Demo from './Switch1.demo.vue';
   import Switch2Demo from './Switch2.demo.vue';
   import Demo from '../Demo.vue';
+  import Doc from './Switch.doc.md';
 
   export default {
     components: { Demo },
-    setup() {return { Switch1Demo, Switch2Demo };}
+    setup() {return { Switch1Demo, Switch2Demo, Doc };}
   };
 </script>
 <style lang="scss" scoped>
